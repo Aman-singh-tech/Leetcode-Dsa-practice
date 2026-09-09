30b819fd603c46797284b23ab74796cdda60e0ab
Aman <bablooaman.p2@gmail.com>
2026-09-09 10:12:15 +0530

    Time: 83 ms (47.54%), Space: 88.9 MB (49.08%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,10 +5,12 @@ public:
         if(nums.size()==0) return 0;
         int longest=1;
         for(int num:st){
-            if(st.find(num-1)==st.end()){
+
+            //this loop gives genine start of sequence if exist 
+            if(st.find(num-1)==st.end()){   //if num-1 is not found then loop chalega  this st.end gives not found in result if element is not in set 
                 int currentNum=num;
                 int currentlen=1;
-                while(st.find(currentNum+1)!=st.end()){
+                while(st.find(currentNum+1)!=st.end()){  // if currentNum is found in set then loop chalega 
                     currentNum++;
                     currentlen++;
                 }
